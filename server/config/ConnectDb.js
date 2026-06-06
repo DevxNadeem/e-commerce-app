5fc04c49641cd1c242f93484dd2ed60d23701203
Nadeem sheikh <ns2798579@gmail.com>
2026-06-06 15:11:19 +0530

    URI rearrangement

--- server/config/ConnectDb.js
+++ server/config/ConnectDb.js
@@ -5,7 +5,7 @@ const URI = process.env.MONGODB_URL;
 const connectDB = async () => { 
     try {
         console.log(URI);
-        await mongoose.connect('mongodb+srv://ns2798579_db_user:SHEIKH123@cluster0.yzeos9z.mongodb.net/e-commerce-app')
+        await mongoose.connect(URI)
         console.log("MongoDB Connected")
     } catch (err) {
         console.log(err)
